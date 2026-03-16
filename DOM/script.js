9bdf830403c1e5216303e72097ba42f631d983a6
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-03-16 22:12:17 +0530

    Website Maker Started

---
+++ DOM/script.js
@@ -0,0 +1,48 @@
+ console.log(" Welcomw to DOM");
+// document.body.children[0].childNodes[5].style.backgroundColor="red"
+// 'red'
+// document.body.childNodes[3].style.he
+
+// undefined
+// document.body.childNodes[3].style.height=330px
+// VM494:1 Uncaught SyntaxError: Invalid or unexpected token
+// document.body.childNodes[3].style.height="330px"
+// '330px'
+// document.body.childNodes[3].style.backgroundColor="green"
+// 'green'
+// document.body.childNodes
+// NodeList(7) [text, div.container, text, div.content, text, script, text]
+// document.body.firstChild
+// #text
+// document.body.firstelChild
+// undefined
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​flex<div class=​"box">​Box​</div>​flex<div class=​"box">​Box​</div>​flex<div class=​"box" style=​"background-color:​ red;​">​Box​</div>​flex<div class=​"box">​Box​</div>​flex<div class=​"box">​Box​</div>​flex</div>​
+// document.body.lastChild
+// #text
+// document.body.firstelChild
+// undefined
+// document.body.lastElementChild
+// <script src=​"script.js">​</script>​
+// document.body.childNodes[1].style.borderRadius="2px solid black",height="150px"
+// '150px'
+// document.body.childNodes[1].style.borderRadius="2px solid black"
+// '2px solid black'
+// document.body.childNodes[1].style.backgroundColor="green"
+// 'green'
+// document.body.childNodes[1].style.borderRadius="2px solid black"
+
+// '2px solid black'
+// document.body.children[0].style.height="150px"
+// '150px'
+// document.querySelector(".container").classList.toggle("red")
+// true
+// document.querySelector(".container").classList.toggle("red")
+// false
+// document.querySelector(".container").classList.toggle("red")
+// true
+// document.querySelector(".container").classList.toggle("red")
+// false
+// document.querySelector(".container").classList.toggle("red")
+// true
+// This all are pratice for dom in browser
