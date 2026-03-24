4f7b9d345b33c7b5cdf983f8c624b3ca3f401298
Himanshu Tripathi <himanshuramtripathi10@gmail.com>
2026-03-24 22:59:25 +0530

    Usage of map done

---
+++ advance js/string.js
@@ -0,0 +1,21 @@
+let a = [12,13,14,15,16];
+let result = a.map((num) => {
+  return num*num;
+}
+)
+let users =[
+   {name:"himanshu",age:"18"},
+    {name:"prince",age:"17"}
+   ]
+let result2 = users.map((user) => {
+  return user.name;
+}
+)
+let result3 = users.map((user) => {
+  return "Mr "+user.name;
+}
+)
+
+console.log(result);
+console.log(result2);
+console.log(result3);
